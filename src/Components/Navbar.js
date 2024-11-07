58b1a678ba9e6ebf80c4ab1c0d23251f3dc8485c
Adithya Jeyaram <adithya.jeyaram@gmail.com>
2024-11-07 18:53:18 +0530

    changed navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,38 +2,50 @@ import React from "react";
 import "../App.css";
 import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom";
-//reused
+
+// Reused
 export const NavBar = () => {
-  const navigate = useNavigate()
-  const onLogout = ()=>{
-    console.log("LoggedOut")
-  }
-  const user = null
+  const navigate = useNavigate();
+  const onLogout = () => {
+    console.log("LoggedOut");
+  };
+  const user = null;
+
   return (
     <div className="Navlink">
       <div className="mt-3">
-        <h1 className="nav-gradient text-purple text-2xl ml-3">Athletix</h1>
+        <h1
+          className="nav-gradient text-purple text-2xl ml-3 cursor-pointer"
+          onClick={() => {
+            navigate("/"); // Navigate to the home page
+          }}
+        >
+          Athletix
+        </h1>
       </div>
       <div className="mt-3 mr-3">
         {user === null ? (
           <Button
             onClick={() => {
-              navigate("/login")
+              navigate("/login");
             }}
-            sx={{ color: "#ffffff", textTransform: "none",borderColor:"#ffffff" }}
+            sx={{ color: "#ffffff", textTransform: "none", borderColor: "#ffffff" }}
           >
             Login
           </Button>
         ) : (
           <Button
             onClick={onLogout}
-            sx={{ color: "#ffffff", textTransform: "none",borderColor:"#ffffff"}}
-          >Logout</Button>
+            sx={{ color: "#ffffff", textTransform: "none", borderColor: "#ffffff" }}
+          >
+            Logout
+          </Button>
         )}
         <Button
           onClick={() => {
+            navigate("/aboutus"); // Navigate to the About Us page
           }}
-          sx={{ color: "#ffffff", textTransform: "none",borderColor:"#ffffff"}}
+          sx={{ color: "#ffffff", textTransform: "none", borderColor: "#ffffff" }}
         >
           About us
         </Button>
